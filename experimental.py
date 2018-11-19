a0cd327c142d930d965cfb54b0bfe9b40d669789
David J. Hammaker <davidjosh93@gmail.com>
2018-11-19 14:23:38 -0500

    Add custom error pages
    Includes 404 and 500 error pages. Both link to the home page.

--- experimental.py
+++ experimental.py
@@ -8,5 +8,15 @@ def index():
     return render_template('index.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True, port='7700')
